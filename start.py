e47c502fd381d4b00d667932c23d1dc30d1df965
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-06-11 10:28:15 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system
-system ("git clone https://github.com/TechnoUserbot && cd userbot && python3 -m userbot")
+system ("git clone -b new https://github.com/TechnoUserbot && cd TechnoUserbot && python3 -m userbot")
 # try af 
-print("OH SYSTEM IS CRASHED")
-system("python3 -m userbot")
+
+
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @TechnoUB")
